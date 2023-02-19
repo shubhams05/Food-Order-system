858d7a0daba50938bda75b9592ee4fee5b5b9d31
shubhams05 <shubhamsingh952160@gmail.com>
2023-02-20 00:18:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginPage from './Components/LoginPage';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+ import MainPage from './Components/MainPage';
+
+function App() {
+  return (
+   <>
+   <div><MainPage/></div>
+   
+   </>
+  )
+}
+
+export default App;
