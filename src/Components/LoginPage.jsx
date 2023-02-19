ccb924e354c9c5ed3b478f0e7d8081aacb74b9f7
shubhams05 <shubhamsingh952160@gmail.com>
2023-02-20 00:25:03 +0530

    change facebook to twitter

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
               <button class="btn btn-lg btn-block btn-primary" style={{backgroundColor:'#dd4b39'}}
                 type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
               <button class="btn btn-lg btn-block btn-primary mb-2" style={{backgroundColor: '#3b5998'}}
-                type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
+                type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with twitter</button>
   
             </div>
           </div>
